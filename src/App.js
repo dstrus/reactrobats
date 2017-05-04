4d5d4fd39cc5a04740a686440b69c7fd92c7803e
Dave Strus <dave@getfretless.com>
2017-05-04 14:44:23 -0400

    Add promotion (single component)

--- src/App.js
+++ src/App.js
@@ -7,27 +7,56 @@ class App extends Component {
     this.acrobat = {
       name: '',
       act: '',
+      className: '',
     }
     this.state = {
-      acrobats: []
+      acrobats: {}
     }
   }
   
   addAcrobat(ev) {
     ev.preventDefault()
-    const acrobats = [...this.state.acrobats]
+    const acrobats = {...this.state.acrobats}
+    const acrobatId = `acrobat-${Date.now()}`;
     const acrobat = {
+      id: acrobatId,
       name: this.acrobatName.value,
       act: this.acrobatAct.value,
+      className: '',
     }
-    acrobats.push(acrobat)
+    acrobats[acrobatId] = acrobat
     this.setState({ acrobats })
     this.acrobatForm.reset()
   }
+
+  promoteAcrobat(ev, acrobat) {
+    ev.preventDefault();
+    const acrobats = {...this.state.acrobats}
+    if (acrobat.className === 'promoted') {
+      acrobat.className = ''
+    } else {
+      acrobat.className =  'promoted'
+    }
+    acrobats[acrobat.id] = acrobat
+    this.setState({ acrobats })
+  }
   
-  renderAcrobat(acrobat, i) {
+  renderAcrobat = (acrobat) => {
     return (
-      <li className="clearfix" key={i}>{acrobat.name}, Master of {acrobat.act}</li>
+      <li className={`clearfix acrobat ${acrobat.className}`} key={acrobat.id}>
+        <span className="acrobat-name">
+          {acrobat.name}, Master of {acrobat.act}
+        </span>
+        
+        <span className="actions expanded button-group">
+          <button
+             className="promote warning button"
+             onClick={(ev) => this.promoteAcrobat(ev, acrobat)}
+          >
+            <i className="fa fa-star"></i>
+          </button>
+        </span>
+      </li>
     )
   }
 
@@ -57,7 +86,9 @@ class App extends Component {
         <div className="row">
           <div className="medium-8 medium-offset-2 columns">
             <ul className="no-bullet">
-              {this.state.acrobats.map(this.renderAcrobat)}
+              {
+                Object.keys(this.state.acrobats)
+                  .map((id) => this.renderAcrobat(this.state.acrobats[id]))}
             </ul>
           </div>
         </div>
