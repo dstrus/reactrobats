f737ae46fe9cc509edbb1f4e2643dde0fd9fdf4e
Dave Strus <dave@getfretless.com>
2017-05-05 12:33:13 -0400

    Refactor AcrobatList to use spread attributes

--- src/AcrobatList.js
+++ src/AcrobatList.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react'
 import Acrobat from './Acrobat.js'
 
 class AcrobatList extends Component {
+  sortAcrobats = (a, b) => {
+    return this.props.acrobats[a].position - this.props.acrobats[b].position
+  }
+
   render() {
     const acrobats = this.props.acrobats
+    const actions = {
+      acrobats: this.props.acrobats,
+      saveAcrobat: this.props.saveAcrobat,
+      editAcrobat: this.props.editAcrobat,
+      removeAcrobat: this.props.removeAcrobat,
+      moveUp: this.props.moveUp,
+      moveDown: this.props.moveDown,
+    }
     return (
       <ul className="no-bullet">
         {
           Object.keys(acrobats)
-            .sort((a, b) => acrobats[a].position - acrobats[b].position)
-            .map((id) => {
-              return (
-                <Acrobat
-                  key={id}
-                  acrobat={acrobats[id]}
-                  acrobats={this.props.acrobats}
-                  saveAcrobat={this.props.saveAcrobat}
-                  editAcrobat={this.props.editAcrobat}
-                  removeAcrobat={this.props.removeAcrobat}
-                  moveUp={this.props.moveUp}
-                  moveDown={this.props.moveDown}
-                />
-              )
-            })
+            .sort(this.sortAcrobats)
+            .map(id => <Acrobat key={id} acrobat={acrobats[id]} {...actions} />)
         }
       </ul>
     )
