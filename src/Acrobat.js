a3382e9d452aa11e4764f6a09d04b1464d32ff5a
Dave Strus <dave@getfretless.com>
2017-05-05 09:25:24 -0400

    Save acrobat prop to const in Acrobat

--- src/Acrobat.js
+++ src/Acrobat.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react'
 
 class Acrobat extends Component {
   render() {
+    const acrobat = this.props.acrobat
     return (
       <li 
-        className={`clearfix acrobat ${this.props.acrobat.className}`}
-        key={this.props.acrobat.id}
+        className={`clearfix acrobat ${acrobat.className}`}
+        key={acrobat.id}
       >
         <span className="acrobat-name">
-          {this.props.acrobat.name}, Master of {this.props.acrobat.act}
+          {acrobat.name}, Master of {acrobat.act}
         </span>
         
         <span className="actions expanded button-group">
           <button
             className="edit button"
-            onClick={ev => this.props.editAcrobat(ev, this.props.acrobat)}
+            onClick={ev => this.props.editAcrobat(ev, acrobat)}
           >
             <i className="fa fa-pencil"></i>
           </button>
           <button
             className="promote warning button"
-            onClick={ev => this.props.promoteAcrobat(ev, this.props.acrobat)}
+            onClick={ev => this.props.promoteAcrobat(ev, acrobat)}
           >
             <i className="fa fa-star"></i>
           </button>
           <button
             className="remove alert button"
-            onClick={ev => this.props.removeAcrobat(ev, this.props.acrobat)}
+            onClick={ev => this.props.removeAcrobat(ev, acrobat)}
           >
             <i className="fa fa-trash-o"></i>
           </button>
